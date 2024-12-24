25351b2f850c2ea11d3d6127f3d2b624137a644e
dlyav <github_090506@mail.ru>
2024-12-24 21:15:21 +0500

    1

---
+++ Tyuiu.RyabtsevNE.Sprint7.Project.V2/LoginForm.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Tyuiu.RyabtsevNE.Sprint7.Project.V2
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ControlDark;
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(panel2);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(389, 450);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Coral;
+            panel2.Controls.Add(label1);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(389, 108);
+            panel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.Dock = DockStyle.Fill;
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(389, 108);
+            label1.TabIndex = 0;
+            label1.Text = "Авторизация";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(91, 178);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(200, 47);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(91, 297);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(200, 47);
+            textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button1.Location = new Point(158, 399);
+            button1.Name = "button1";
+            button1.Size = new Size(63, 39);
+            button1.TabIndex = 3;
+            button1.Text = "Войти";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.ControlLightLight;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label2.Location = new Point(167, 154);
+            label2.Name = "label2";
+            label2.Size = new Size(54, 21);
+            label2.TabIndex = 4;
+            label2.Text = "Логин";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = SystemColors.ControlLightLight;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label3.Location = new Point(158, 273);
+            label3.Name = "label3";
+            label3.Size = new Size(63, 21);
+            label3.TabIndex = 5;
+            label3.Text = "Пароль";
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(389, 450);
+            Controls.Add(panel1);
+            Name = "LoginForm";
+            Text = "LoginForm";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Label label1;
+        private Button button1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label3;
+        private Label label2;
+    }
+}
---
+++ Tyuiu.RyabtsevNE.Sprint7.Project.V2/MForm.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace Tyuiu.RyabtsevNE.Sprint7.Project.V2
+{
+    partial class MForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
+            textBox10 = new TextBox();
+            textBox11 = new TextBox();
+            textBox12 = new TextBox();
+            textBox13 = new TextBox();
+            textBox14 = new TextBox();
+            textBox15 = new TextBox();
+            textBox16 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ControlDark;
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(textBox16);
+            panel1.Controls.Add(textBox15);
+            panel1.Controls.Add(textBox14);
+            panel1.Controls.Add(textBox13);
+            panel1.Controls.Add(textBox12);
+            panel1.Controls.Add(textBox11);
+            panel1.Controls.Add(textBox10);
+            panel1.Controls.Add(textBox9);
+            panel1.Controls.Add(textBox8);
+            panel1.Controls.Add(textBox7);
+            panel1.Controls.Add(textBox6);
+            panel1.Controls.Add(textBox5);
+            panel1.Controls.Add(textBox4);
+            panel1.Controls.Add(textBox3);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(panel2);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1291, 620);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.ControlLightLight;
+            panel2.Controls.Add(label1);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1291, 87);
+            panel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label1.Location = new Point(515, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(340, 58);
+            label1.TabIndex = 0;
+            label1.Text = "Сеть магазинов";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(3, 113);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(379, 23);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "                                    Каталог товаров                                   >";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(3, 186);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(379, 23);
+            textBox2.TabIndex = 2;
+            textBox2.Text = "категория товара ";
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(3, 447);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(379, 23);
+            textBox3.TabIndex = 3;
+            textBox3.Text = "категория товара ";
+            textBox3.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(3, 418);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(379, 23);
+            textBox4.TabIndex = 4;
+            textBox4.Text = "категория товара ";
+            textBox4.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(3, 273);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(379, 23);
+            textBox5.TabIndex = 5;
+            textBox5.Text = "категория товара ";
+            textBox5.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(3, 389);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(379, 23);
+            textBox6.TabIndex = 6;
+            textBox6.Text = "категория товара ";
+            textBox6.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(3, 360);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(379, 23);
+            textBox7.TabIndex = 7;
+            textBox7.Text = "категория товара ";
+            textBox7.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(3, 331);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(379, 23);
+            textBox8.TabIndex = 8;
+            textBox8.Text = "категория товара ";
+            textBox8.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(3, 302);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(379, 23);
+            textBox9.TabIndex = 9;
+            textBox9.Text = "категория товара ";
+            textBox9.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(3, 244);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(379, 23);
+            textBox10.TabIndex = 10;
+            textBox10.Text = "категория товара ";
+            textBox10.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(3, 215);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(379, 23);
+            textBox11.TabIndex = 11;
+            textBox11.Text = "категория товара ";
+            textBox11.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox12
+            // 
+            textBox12.Location = new Point(3, 534);
+            textBox12.Name = "textBox12";
+            textBox12.Size = new Size(379, 23);
+            textBox12.TabIndex = 12;
+            textBox12.Text = "категория товара ";
+            textBox12.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox13
+            // 
+            textBox13.Location = new Point(3, 505);
+            textBox13.Name = "textBox13";
+            textBox13.Size = new Size(379, 23);
+            textBox13.TabIndex = 13;
+            textBox13.Text = "категория товара ";
+            textBox13.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox14
+            // 
+            textBox14.Location = new Point(3, 476);
+            textBox14.Name = "textBox14";
+            textBox14.Size = new Size(379, 23);
+            textBox14.TabIndex = 14;
+            textBox14.Text = "категория товара ";
+            textBox14.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox15
+            // 
+            textBox15.Location = new Point(891, 113);
+            textBox15.Name = "textBox15";
+            textBox15.Size = new Size(379, 23);
+            textBox15.TabIndex = 15;
+            textBox15.Text = "Стоимость";
+            textBox15.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox16
+            // 
+            textBox16.Location = new Point(439, 113);
+            textBox16.Name = "textBox16";
+            textBox16.Size = new Size(379, 23);
+            textBox16.TabIndex = 16;
+            textBox16.Text = "Товар";
+            textBox16.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.ControlLightLight;
+            label2.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label2.Location = new Point(1196, 563);
+            label2.Name = "label2";
+            label2.Size = new Size(83, 17);
+            label2.TabIndex = 17;
+            label2.Text = "Связьс нами";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = SystemColors.ControlLightLight;
+            label3.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label3.Location = new Point(1167, 594);
+            label3.Name = "label3";
+            label3.Size = new Size(112, 17);
+            label3.TabIndex = 18;
+            label3.Text = "+7(999)999-99-99";
+            // 
+            // MForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1291, 620);
+            Controls.Add(panel1);
+            Name = "MForm";
+            Text = "MForm";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label1;
+        private Label label3;
+        private Label label2;
+        private TextBox textBox16;
+        private TextBox textBox15;
+        private TextBox textBox14;
+        private TextBox textBox13;
+        private TextBox textBox12;
+        private TextBox textBox11;
+        private TextBox textBox10;
+        private TextBox textBox9;
+        private TextBox textBox8;
+        private TextBox textBox7;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+    }
+}
