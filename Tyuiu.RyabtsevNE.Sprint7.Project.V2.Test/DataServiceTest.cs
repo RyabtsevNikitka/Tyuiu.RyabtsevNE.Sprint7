7b08273ad7d8e1aba3adc248758191a6b734bf6f
dlyav <dlyav@N3KITOS1K>
2024-12-24 17:22:22 +0500

    Add project files.

---
+++ Tyuiu.RyabtsevNE.Sprint7.Project.V2.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.RyabtsevNE.Sprint7.Project.V2.Lib;
+
+namespace Tyuiu.RyabtsevNE.Sprint7.Project.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
